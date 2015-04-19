14b5831f8a080e058d7ca31f49a5b697b1c18ca6
a-santamaria <chino.sebastian@gmail.com>
2015-04-19 00:31:42 -0400

    changed create pet and validate user with new db (#7)

--- create_pet.php
+++ create_pet.php
@@ -5,17 +5,20 @@
  * atributes read from HTTP Post Request
  */
 
-//import db connection file
+/* import user creation */
+include 'create_user.php';
+/* import db connection file */
 include 'db_connect.php';
 
 
-
+$username = NULL;
 $name = NULL;
 $type = NULL;
 $owner = NULL;
 $profilePic = NULL;
 
 
+if(isset($_POST["username"])) $username = $_POST["username"];
 if(isset($_POST["name"])) $name = $_POST["name"];
 if(isset($_POST["type"])) $type = $_POST["type"];
 if(isset($_POST["owner"])) $owner = $_POST["owner"];
@@ -23,17 +26,25 @@ if(isset($_POST["owner"])) $owner = $_POST["owner"];
 
 
 
-// Create connection to mysql database
+/* Create connection to mysql database */
 $conn = dbConnect();
 
-//MySql query inseting new row in Pet
-$mysql_query = "INSERT INTO Pet(name, type, owner_username)
-                        VALUES('$name', '$type','$owner')";
+/* MySql query inseting new row in Pet */
+$mysql_query = "INSERT INTO Pet(username, name, type, owner_username)
+                        VALUES('$username', '$name', '$type','$owner')";
 
-if($conn->query($mysql_query) == TRUE){
-   echo "New Pet created";
+/* insert new user in table User*/
+$userCreated = createUser($conn, $username, NULL);
+
+if($userCreated){
+  if($conn->query($mysql_query) == TRUE){
+    echo "New Pet created";
+  }else{
+    echo "Error inserting new Pet in database";
+  }
 }else{
-  echo "Error inserting new Pet in database";
+  header('status : 0');
+  echo "Error inserting new User in database";
 }
 
 ?>
