2339bf0c04016029bb1eaed499a2f50fde070760
a-santamaria <chino.sebastian@gmail.com>
2015-04-20 19:37:51 -0500

    fixed log in to work with json response from php service

--- app/src/main/java/com/example/david/rawr/db/ValidateUser.java
+++ app/src/main/java/com/example/david/rawr/db/ValidateUser.java
@@ -9,9 +9,14 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -27,6 +32,7 @@ public class ValidateUser extends AsyncTask<String, Integer, String> {
     private String pass;
     private HttpResponse response;
     private static String url_validate_user = "http://178.62.233.249/rawr/validate_user.php";
+    private JSONObject jsonResponse;
 
     public ValidateUser(String user, String pass) {
         this.user = user;
@@ -38,29 +44,48 @@ public class ValidateUser extends AsyncTask<String, Integer, String> {
      */
 
     protected String doInBackground(String... args) {
-
-        // Building Parameters
-        List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("username", this.user));
-        params.add(new BasicNameValuePair("password", this.pass));
-
+        String status = null;
+        JSONObject jsonObjSend = new JSONObject();
+        try {
+            jsonObjSend.put("username", this.user);
+            jsonObjSend.put("password", this.pass);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         HttpClient client = new DefaultHttpClient();
         HttpPost post = new HttpPost(url_validate_user);
-        String responseValue = "";
+
+        post.setHeader("Accept", "application/json");
+        post.setHeader("Content-type", "application/json");
+        //post.setHeader(HTTP.CONTENT_TYPE, "application/json; charset=utf-8");
+
+
+        jsonResponse = null;
+        StringEntity se = null;
         try {
-            post.setEntity(new UrlEncodedFormEntity(params));
+            se = new StringEntity(jsonObjSend.toString());
+            post.setEntity(se);
+
+            HttpResponse response;
             response = client.execute(post);
-            Header header = response.getFirstHeader("Content-Length");
-            Log.i(header.getName(), header.getValue());
-            responseValue = header.getValue();
+            JsonParser jsonParser = new JsonParser(response.getEntity().getContent());
+
+            jsonResponse= jsonParser.getjObject();
+            status = jsonResponse.getString("status");
+            Log.i("Response ", jsonResponse.getString("status"));
+
+            Log.i("Json resoponse","<JSONObject>\n"+jsonResponse.toString()+"\n</JSONObject>");
+
         } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         } catch (ClientProtocolException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return responseValue;
+        return status;
     }
 
     /**
@@ -71,4 +96,8 @@ public class ValidateUser extends AsyncTask<String, Integer, String> {
 
     }
 
+
+    public JSONObject getJsonResponse() {
+        return jsonResponse;
+    }
 }
