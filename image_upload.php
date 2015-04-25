9a2a1e7b6fa78ef23a38566a15e87190982fda05
David Calle <davidcalle9430@gmail.com>
2015-04-25 12:43:17 -0500

    changed saving directory

--- image_upload.php
+++ image_upload.php
@@ -10,7 +10,7 @@
     $binary=base64_decode($base);
     header('Content-Type: bitmap; charset=utf-8');
     // Images will be saved under 'www/imgupload/uplodedimages' folder
-    $file = fopen('uploadedimages/'.$filename . '.' .$extension, 'wb');
+    $file = fopen('static/'.$filename . '.' .$extension, 'wb');
     // Create File
     fwrite($file, $binary);
     fclose($file);
