5736ac7ac355132a359c48cb734a5ec1060a60bd
David Calle <davidcalle9430@gmail.com>
2015-04-25 12:55:14 -0500

    debugging #1

--- image_upload.php
+++ image_upload.php
@@ -1,18 +1,27 @@
 <?php
-    
+    echo "stating method";
     $base=file_get_contents('php://input');
+     echo "got base";
     // Get file name posted from Android App
     $filename = $_REQUEST['filename'];
+
     $extension= $_REQUEST['extension'];
     $username= $_REQUEST['username'];
-    $filename = "asdad";//tempnam('/static', '');
+
+    $filename = tempnam('/static', '');
+    
+    echo "got filename";
+
     // Decode Image
     $binary=base64_decode($base);
+     echo "de encoded";
     header('Content-Type: bitmap; charset=utf-8');
     // Images will be saved under 'www/imgupload/uplodedimages' folder
     $file = fopen('static/'.$filename . '.' .$extension, 'w');
     // Create File
+    echo "opened file";
     fwrite($file, $binary);
+     echo "written file";
     fclose($file)
-    
+     echo "closed file";
 ?>
