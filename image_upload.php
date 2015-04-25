cf2d3b667b210e199d0bf375027d5a6eece2ae20
David Calle <davidcalle9430@gmail.com>
2015-04-25 15:06:59 -0500

    Fix extension error

--- image_upload.php
+++ image_upload.php
@@ -25,9 +25,10 @@
     fclose($file);
 
     $conn = dbConnect();
+    $filename= $filename . $extension;
     $mysql_query = "INSERT INTO Photo(path, username)
                         VALUES('$filename', '$username')";
-   $filename= $filename . $extension;
+   
     if($conn->query($mysql_query) == TRUE){
         $json_return= array('status' => '1');
         echo json_encode($json_return);
