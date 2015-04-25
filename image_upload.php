48ce980ad6fe81d6ec2e13b35fe12aded2ba4a86
David Calle <davidcalle9430@gmail.com>
2015-04-25 14:53:51 -0500

    add JSON response

--- image_upload.php
+++ image_upload.php
@@ -1,16 +1,21 @@
 <?php
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
-    $base=file_get_contents('php://input');
-    $base = str_replace(' ', '+', $base);
-    echo $base;
-    $extension= "png";
+
+
+    $request_body = file_get_contents('php://input');
+    $json_array = json_decode($request_body, true);
+
+    $base=$request_body['photo'];
+    $extension= $request_body['extension'];
+    $username=  $request_body['username'];
 
 
     $filename = tempnam('/static', '');
+    $base = str_replace(' ', '+', $base);
     $base = str_replace('data:image/'.$extension.';base64,', '', $base);
     $binary=base64_decode($base, TRUE);
-    echo $binary;
+
     
     $file = fopen('static'.$filename. '.'. $extension, 'wb');
     // Create File
@@ -18,5 +23,16 @@
     fwrite($file, $binary);
     
     fclose($file);
+
+    $conn = dbConnect();
+    $mysql_query = "INSERT INTO Photo(path, route)
+                        VALUES('$filename', '$username')";
+    if($conn->query($mysql_query) == TRUE){
+        $json_return= array('status' => '1');
+        echo json_encode($json_return);
+    }else{
+        $json_return= array('status' => '0');
+        echo json_encode($json_return);
+    }
     
 ?>
