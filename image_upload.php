719ddec827b5ee4ab8f9de181684cc1e8c821769
David Calle <davidcalle9430@gmail.com>
2015-04-25 14:19:05 -0500

    debugging #17

--- image_upload.php
+++ image_upload.php
@@ -3,16 +3,13 @@
     ini_set('display_errors', 1);
     $base=file_get_contents('php://input');
     $base = str_replace(' ', '+', $base);
-    
-    // Get file name posted from Android App
-    $filename = "mi_archivo";
+    echo $base;
     $extension= "png";
-    $username= "disney";
     $filename = tempnam('/static', '');
-    // Decode Image
+    
     $binary=base64_decode($base, TRUE);
     echo $binary;
-    // Images will be saved under 'www/imgupload/uplodedimages' folder
+    
     $file = fopen('static'.$filename. '.'. $extension, 'wb');
     // Create File
    
