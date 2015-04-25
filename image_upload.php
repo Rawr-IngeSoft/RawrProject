cafbb70917b56ec57fbe7ae595e06ea12ab3ab2e
David Calle <davidcalle9430@gmail.com>
2015-04-25 12:14:21 -0500

    added file extension

--- image_upload.php
+++ image_upload.php
@@ -3,15 +3,16 @@
     $base=$_REQUEST['image'];
     // Get file name posted from Android App
     $filename = $_REQUEST['filename'];
+    $extension= $_REQUEST['extension'];
     
     $filename = tempnam('/static', '');
     // Decode Image
     $binary=base64_decode($base);
     header('Content-Type: bitmap; charset=utf-8');
     // Images will be saved under 'www/imgupload/uplodedimages' folder
-    $file = fopen('uploadedimages/'.$filename, 'wb');
+    $file = fopen('uploadedimages/'.$filename . '.' .$extension, 'wb');
     // Create File
     fwrite($file, $binary);
     fclose($file);
-    echo 'Image upload complete, Please check your php file directory';
+    
 ?>
