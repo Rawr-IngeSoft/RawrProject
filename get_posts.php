b67b9b9c035ce8506e28342a6714a49d6467e0ae
David Calle <davidcalle9430@gmail.com>
2015-04-26 17:23:20 -0500

    SQL correction

--- get_posts.php
+++ get_posts.php
@@ -8,17 +8,17 @@ header("Content-Type: application/json; charset=UTF-8");
 $conn = dbConnect();
 $username = $_GET['username']; // esto debería cambiarse por el id pet
 $sql =
-"SELECT p.idPost, p.username, p.text, p.date,  ph.path, p.status, p.price 
+"SELECT p.idPost, p.username, p.text, p.date,  ph.path, p.type, p.status, p.price 
  FROM Post p, Photo ph
- WHERE  p.idPhoto = ph.idPhoto AND username in 
- 			(
- 			SELECT username_friend 
- 			FROM Friends 
- 			WHERE username = '$username'
- 			UNION  
- 			SELECT username
- 			FROM Friends 
- 			WHERE username_friend = '$username') LIMIT 100";
+ WHERE  p.idPhoto = ph.idPhoto AND p.username in 
+            (
+            SELECT username_friend 
+            FROM Friends 
+            WHERE username = '$username'
+            UNION  
+            SELECT username
+            FROM Friends 
+            WHERE username_friend = '$username') LIMIT 100";
 
 // ahora toca recorrer el query
 $result = $conn->query($sql);
@@ -28,13 +28,12 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
     
        // Crear un diccionario de Post
-    	$arreglo = array('id'=>$row['idPost'], 'text'=>$row['text'], 'date'=>$row['date'], 'idPet'=>$row['idPet'], 'Business'=>$row['Business_username'],'photo'=>$row['path']);
-    	array_push($retorno, $arreglo);
+        $arreglo = array('id'=>$row['idPost'], 'text'=>$row['text'], 'date'=>$row['date'], 'idPet'=>$row['idPet'], 'Business'=>$row['Business_username'],'photo'=>$row['path']);
+        array_push($retorno, $arreglo);
 
     }
     $json = array('posts'=>$retorno);
     echo json_encode($json);
  }else{
- 	echo 'pasó algo muy raro';
+    echo 'pasó algo muy raro';
  }
-
