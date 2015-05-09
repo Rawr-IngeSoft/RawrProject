1d3222da6298becd3a234dcd6da80bb997a42150
David Calle <davidcalle9430@gmail.com>
2015-05-09 08:39:57 -0500

    Get Photo id

--- api/posts/index.php
+++ api/posts/index.php
@@ -1,4 +1,15 @@
 <?php
+function obtenerIdPhoto($username, $path) {
+   	$conn = dbConnect();
+   	$sql =
+    	"SELECT id
+     	 FROM Photo
+     	 WHERE username='$username' AND path ='$path'";
+	$result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    return $row['id'];
+}
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 include '../../db_connect.php';
@@ -7,7 +18,16 @@ if($_SERVER['REQUEST_METHOD']=="GET"){
 	$conn = dbConnect();
 	echo "hicieron un request GET";
 }elseif($_SERVER['REQUEST_METHOD']=="POST"){
-	echo "hicieron un request POST";
+	$request_body = file_get_contents('php://input');
+	$json_array = json_decode($request_body, true);
+	//sacar del JSON array los datos que necesito
+	$username= $json_array['username'];
+	$text= $json_array['text'];
+	$path= $json_array['path'];
+	$type= $json_array['type'];
+	$id= obtenerIdPhoto($username, $path);
+	echo "el id de la foto es ".$id;
+
 }elseif ($_SERVER['REQUEST_METHOD']=="PUT") {
 	echo "hicieron un request PUT";
 }elseif($_SERVER['REQUEST_METHOD']=="DELETE"){
