87ea0288e75f02d5f1c691e7fe414700257bf311
lespoir <david.avila@javeriana.edu.co>
2015-05-14 22:26:42 -0500

    adding sound to notifications

--- app/src/main/java/com/example/david/rawr/Services/Chat_service.java
+++ app/src/main/java/com/example/david/rawr/Services/Chat_service.java
@@ -7,12 +7,15 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.example.david.rawr.IRemoteService;
 import com.example.david.rawr.MainActivities.Chat_window;
+import com.example.david.rawr.Models.Pet;
 import com.example.david.rawr.R;
 import com.example.david.rawr.Models.Message;
 import com.github.nkzawa.emitter.Emitter;
@@ -25,6 +28,7 @@ import org.json.JSONObject;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -38,6 +42,10 @@ public  class Chat_service extends Service {
     String petUsername;
     ArrayList<String> friendsList;
     NotificationManager notificationManager;
+    NotificationCompat.Builder notificationBuilder;
+    HashMap<String,String> notifications;
+    NotificationCompat.InboxStyle inboxStyle;
+
     IRemoteService.Stub myBinder = new IRemoteService.Stub() {
         @Override
         public List<String> getFriendsList() throws RemoteException {
@@ -75,14 +83,25 @@ public  class Chat_service extends Service {
     // Binder for communication
     @Override
     public void onCreate() {
-        Log.e("status","on_create");
         super.onCreate();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         try {
-            Log.e("status","starting_service");
+            notifications = new HashMap<>();
+            Intent intentNotification = new Intent(getApplicationContext(), Chat_window.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),0, intentNotification,0);
+            notificationManager = (NotificationManager)
+                    getSystemService(NOTIFICATION_SERVICE);
+            inboxStyle = new NotificationCompat.InboxStyle();
+            notificationBuilder = new NotificationCompat.Builder(Chat_service.this)
+                    .setSmallIcon(R.drawable.logo_icon)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .setDefaults(Notification.DEFAULT_VIBRATE)
+                    .setStyle(inboxStyle)
+                    .setSound(Uri.parse("android.resource://" + getApplicationContext().getPackageName() + "/" + R.raw.notification_sound));
             mySocket = IO.socket("http://178.62.233.249:3000");
             JSONObject data = new JSONObject();
             SharedPreferences sharedPreferences;
@@ -109,25 +128,17 @@ public  class Chat_service extends Service {
                 @Override
                 public void call(Object... args) {
                     try {
-                        Log.e("status", "listen_msg");
                         JSONObject data = (JSONObject)args[0];
-                        Intent intent = new Intent(getApplicationContext(), Chat_window.class);
-                        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),0, intent,0);
-                        Message msg = new Message((String)data.get("message"),(String)data.get("sender"));
-                        Notification notification = new Notification.Builder(Chat_service.this)
-                                    .setSmallIcon(R.drawable.logo_icon)
-                                    .setContentTitle(msg.getPerson())
-                                    .setContentText(msg.getMessage())
-                                    .setAutoCancel(true)
-                                    .setContentIntent(pendingIntent).build();
-                        notificationManager.notify(0,notification);
+                        Log.e("status", data.getString("message"));
+                        inboxStyle.addLine(data.getString("message"));
+                        notificationBuilder.setContentTitle(data.getString("sender"));
+                        notificationManager.notify(0,notificationBuilder.build());
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 }
             };
-            notificationManager = (NotificationManager)
-                    getSystemService(NOTIFICATION_SERVICE);
+
             petUsername=sharedPreferences.getString("petUsername", "");
             data.put("username", petUsername);
             friendsList= new ArrayList();
