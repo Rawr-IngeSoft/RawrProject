a0ccf8385749ee88fd035cc80c8f0f668738f650
a-santamaria <chino.sebastian@gmail.com>
2015-05-16 23:55:04 -0500

    added answer request

---
+++ answer_request.php
@@ -0,0 +1,59 @@
+<?php
+include 'db_connect.php';
+// header para mostrar que se va a recbir un JSON
+header("Content-Type: application/json; charset=UTF-8");
+// Create connection to mysql database
+$conn = dbConnect();
+
+$request_body = file_get_contents('php://input');
+
+$json_array = json_decode($request_body, true);
+
+$sender= $json_array['sender'];
+$receiver=$json_array['receiver'];
+$status= $json_array['status'];
+
+$sql="SELECT 1 FROM Request
+      WHERE username_sender = '$sender' AND username_receiver = '$receiver'";
+$result = $conn->query($sql);
+if($result > 0){
+
+  if($status == 'rejected'){
+    $sql="UPDATE Request SET status='$status'
+          WHERE username_sender='$sender' AND username_receiver='$receiver'";
+    if($conn->query($sql) == TRUE)
+      $json_return= array('status' => '1');
+    else
+      $json_return= array('status' => '0');
+    echo json_encode($json_return);
+
+  }else if ($status == 'accepted'){
+    $sql="UPDATE Request SET status='$status'
+          WHERE username_sender='$sender' AND username_receiver='$receiver'";
+
+    if($conn->query($sql) == TRUE){
+      $sql="INSERT INTO Friends(username, username_friend)
+            VALUES ('$sender', '$receiver')";
+      if($conn->query($sql) == TRUE)
+        $json_return= array('status' => '1');
+      else
+        $json_return= array('status' => '0');
+      echo json_encode($json_return);
+    }else{
+
+      //roll back
+      $sql="UPDATE Request SET status='pending'
+            WHERE username_sender='$sender' AND username_receiver='$receiver'";
+
+      $json_return= array('status' => '0');
+      echo json_encode($json_return);
+    }
+  }else {//bad request status wrong
+    $json_return= array('status' => '0');
+    echo json_encode($json_return);
+  }
+
+}else{//request doesn't exist
+  $json_return= array('status' => '0');
+  echo json_encode($json_return);
+}
