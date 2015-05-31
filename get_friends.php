80b3e67e9abd96b9e2263eae95fd4b3a0f1808ce
David Calle <davidcalle9430@gmail.com>
2015-05-30 21:08:58 -0500

    added friends picture

--- get_friends.php
+++ get_friends.php
@@ -8,17 +8,15 @@ header("Content-Type: application/json; charset=UTF-8");
 $conn = DB::dbConnect();
 $username = $_GET['username']; // esto debería cambiarse por el id pet
 $sql =
-    "SELECT p.username, p.owner_username, p.name, p.type, p.race, p.birth_date, p.gender
-     FROM Friends f, Pet p
-     WHERE f.username_friend='$username' AND f.username=p.username";
-
+    "SELECT p.username, p.owner_username, p.name, p.type, p.race, p.birth_date, p.gender, ph.path
+     FROM Friends f, Pet p, User u LEFT JOIN Photo ph on ph.idPhoto = u.idPhoto_profile
+     WHERE f.username_friend='$username' AND f.username=p.username AND u.username ='$username'";
 // ahora toca recorrer el query
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     $retorno= array();
     while($row = $result->fetch_assoc()) {
-
        // Crear un diccionario de Post
         $arreglo = array(
             "username"=>$row['username'],
@@ -27,10 +25,10 @@ if ($result->num_rows > 0) {
             "type"=> $row['type'],
             "race"=> $row['race'],
             "birth_date"=> $row['birth_date'],
-            "gender"=> $row['gender']
+            "gender"=> $row['gender'],
+            "path"=>$row['path']
             );
         array_push($retorno, $arreglo);
-
     }
     $json = array('friends'=>$retorno, "status"=>"1");
     echo json_encode($json);
